48f9916913cc18bfc57d735194064c17e16d49b2
Garazd <GarazdVZ@gmail.com>
2017-06-14 23:34:45 +0300

    Change List to Set, change owner in the table

--- src/main/java/com/spares/entity/Spares.java
+++ src/main/java/com/spares/entity/Spares.java
@@ -3,7 +3,7 @@ package com.spares.entity;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -12,13 +12,14 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Table(name = "spares")
@@ -32,20 +33,21 @@ public class Spares {
     @Id
     @SequenceGenerator(name = "generator", sequenceName = "spares_id_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generator")
-    @Column(name = "id", unique = true, nullable = false)
+    @Column(name = "spares_id", unique = true, nullable = false)
     private UUID id;
 
     @Column(name = "name", nullable = false)
-    @NotEmpty
     private String name;
 
     @Column(name = "number", nullable = false)
-    @NotEmpty
     private Long number;
 
     @Column(name = "description")
     private String description;
 
-    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "spares", cascade = CascadeType.ALL)
-    private List<Car> cars;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "spares_car",
+        joinColumns = @JoinColumn(name = "spares_id", nullable = false),
+        inverseJoinColumns = @JoinColumn(name = "car_id", nullable = false))
+    private Set<Car> cars;
 }
