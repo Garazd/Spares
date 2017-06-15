5585289215b2d1fbcd465c811746aa4503d9edc5
Garazd <GarazdVZ@gmail.com>
2017-06-15 22:17:26 +0300

    Add interface CarRepository

---
+++ src/main/java/com/spares/repository/CarRepository.java
@@ -0,0 +1,10 @@
+package com.spares.repository;
+
+import com.spares.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+}
