d1869486dd39cb1f39cc271a76f4021d18baabbe
Garazd <GarazdVZ@gmail.com>
2017-06-19 22:04:57 +0300

    Create class CarServiceImpl

---
+++ src/main/java/com/spares/service/CarServiceImpl.java
@@ -0,0 +1,37 @@
+package com.spares.service;
+
+import com.spares.entity.Car;
+import com.spares.repository.CarRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CarServiceImpl implements CarService {
+
+    @Autowired
+    CarRepository carRepository;
+
+    @Override
+    public Car create(Car car) {
+        return carRepository.saveAndFlush(car);
+    }
+
+    @Override
+    public Car update(Car car) {
+        return carRepository.saveAndFlush(car);
+    }
+
+    @Override
+    public void delete(Long id) {
+        carRepository.delete(id);
+    }
+
+    @Override
+    public Car getById(Long id) {
+        return carRepository.findOne(id);
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return carRepository.findAll();
+    }
+}
