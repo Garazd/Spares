dd31488b00760eac549b83a934c5e593cbb00c7d
Garazd <GarazdVZ@gmail.com>
2017-06-20 22:19:54 +0300

    Add annotation

--- src/main/java/com/spares/service/CarServiceImpl.java
+++ src/main/java/com/spares/service/CarServiceImpl.java
@@ -4,7 +4,9 @@ import com.spares.entity.Car;
 import com.spares.repository.CarRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CarServiceImpl implements CarService {
 
     @Autowired
--- src/main/java/com/spares/service/SparesServiceImpl.java
+++ src/main/java/com/spares/service/SparesServiceImpl.java
@@ -4,7 +4,9 @@ import com.spares.entity.Spares;
 import com.spares.repository.SparesRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SparesServiceImpl implements SparesService {
 
     @Autowired
