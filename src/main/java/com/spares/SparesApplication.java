1cb316a410e38eca84ad96d5192a6cc13c339b8a
Garazd <GarazdVZ@gmail.com>
2017-06-30 23:01:34 +0300

    Add annotation EnableTransactionManagement and extends SpringBootServletInitializer

--- src/main/java/com/spares/SparesApplication.java
+++ src/main/java/com/spares/SparesApplication.java
@@ -1,14 +1,23 @@
 package com.spares;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan("com.spares")
-public class SparesApplication {
+@EnableTransactionManagement
+public class SparesApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SparesApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SparesApplication.class);
     }
 }
