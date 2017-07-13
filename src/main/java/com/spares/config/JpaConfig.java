d345bba061b8f8fc507c957099db5da2e8eff72b
Garazd <GarazdVZ@gmail.com>
2017-07-13 22:18:47 +0300

    Delete profile and add bean JdbcTemplate

--- src/main/java/com/spares/config/JpaConfig.java
+++ src/main/java/com/spares/config/JpaConfig.java
@@ -6,9 +6,9 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -18,7 +18,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories("com.spares.repository")
-@Profile("prod")
 public class JpaConfig {
 
     @Value("${datasource.driver.class.name}")
@@ -46,6 +45,11 @@ public class JpaConfig {
         dataSource.setUsername(username);
         dataSource.setPassword(password);
         return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(configureDataSource());
     }
 
     @Bean
