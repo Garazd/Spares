4eb9d57d37f8eceef5011e7f8711d3a76719c6d9
Garazd <GarazdVZ@gmail.com>
2017-07-13 22:18:00 +0300

    Delete annotation

--- src/main/java/com/spares/SparesApplication.java
+++ src/main/java/com/spares/SparesApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan("com.spares")
-@EnableTransactionManagement
 public class SparesApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
